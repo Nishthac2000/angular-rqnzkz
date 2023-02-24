4c57d6b2c5a0b1ab80abffc70a31b48d37e7d0d8
Nishthac2000 <112468493+Nishthac2000@users.noreply.github.com>
2023-02-24 18:30:06 +0530

    1 file modified, 2 deleted

--- src/app/MyComponents/home-page/home-page.component.ts
+++ src/app/MyComponents/home-page/home-page.component.ts
@@ -1,54 +1,48 @@
-import { Component , OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Team } from 'src/app/Models/team';
 import { RapidAPIService } from 'src/app/services/rapid-api.service';
-
-import {FormControl , FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
-export class HomePageComponent implements OnInit{
-  
-  constructor(private rapidAPi : RapidAPIService){}
+export class HomePageComponent implements OnInit {
+  constructor(private rapidAPi: RapidAPIService) {}
 
-  teams : Team[] = [];
-  
-  teamsList : Team[] = [];
-  
+  teams: Team[] = [];
+
+  teamsList: Team[] = [];
+
   // teamId!: number;
 
   form = new FormGroup({
     //TODO
-    teamName : new FormControl("1")
-  })
+    teamName: new FormControl('1'),
+  });
 
-
-  onSubmit(){
+  onSubmit() {
     // console.log(this.form.controls.teamName.value);
-    
+
     console.log(this.teams);
     this.teams.forEach((team) => {
       //TODO
-      if(team.id == 1){
+      if (team.id == 1) {
         // console.log(team);
         this.teamsList.push(team);
       }
-      
-    })
-    
+    });
   }
 
   ngOnInit(): void {
     this.loadTeams();
-    
   }
 
-  loadTeams(){
-    this.rapidAPi.getTeamData().subscribe(( res : any) => {
+  loadTeams() {
+    this.rapidAPi.getTeamData().subscribe((res: any) => {
       this.teams = res.data;
-      
-    })
+    });
   }
 }
